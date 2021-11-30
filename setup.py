ff8a1fc94325dc8d46e2442b503fd9041455168f
ChenyuZhang16 <goodzhcy@gmail.com>
2021-11-30 13:29:39 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cache_manager",
+    version="0.0.1",
+    author="Chenyu Zhang",
+    author_email="goodzhcy@gmail.com",
+    description="A small python package for managing (downloading) cache files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChenyuZhang16/cache_manager",
+    project_urls={
+        "Bug Tracker": "https://github.com/ChenyuZhang16/cache_manager/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
